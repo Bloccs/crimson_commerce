330524fa69ba048d87d77a5d208145664c3c96b5
Allan MacGregor <amacgregor@allanmacgregor.com>
2018-11-25 12:37:11 -0500

    chore: cleanup against mix format

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule CrimsonCommerce.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
